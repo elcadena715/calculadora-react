2df4aafb90524ee6b7e78004d3936ee437e9d043
elcadena715 <cadensamuel715@gmail.com>
2025-08-04 18:11:43 -0500

    Creacion y diseño de la calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const agregarInput = (valor) => {
+    setInput(input + valor);
+  };
+
+  const limpiar = () => {
+    setInput('');
+  };
+
+  const calcularResultado = () => {
+    try {
+      setInput(eval(input));
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculadora">
+      <div className="display">{input || "0"}</div>
+      <div className="teclado">
+        {"123+456-789*0./".split('').map((val) => (
+          <button onClick={() => agregarInput(val)} key={val}>{val}</button>
+        ))}
+        <button onClick={limpiar}>C</button>
+        <button onClick={calcularResultado}>=</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
